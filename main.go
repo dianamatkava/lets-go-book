7df9e637dddc0264a9a3b95abdad2deb98e3a76f
dianamatkava <88348384+dianamatkava@users.noreply.github.com>
2026-03-20 15:01:55 +0100

    init go server

--- main.go
+++ main.go
@@ -1,96 +1,43 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"log"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
-	// "errors"
 )
 
-// models
-type BoredList struct {
-	Activity      string  `json: "activity"`
-	Type          string  `json: "type"`
-	Participants  int     `json: "participants"`
-	Price         float64 `json: "price"`
-	Link          string  `json: "link"`
-	Key           string  `json: "key"`
-	Accessibility float32 `json: "accessibility"`
-}
 
-// database
-var boredList = []BoredList{}
-var chachedBoredList = []BoredList{}
-
-// views
-func getMyBoredList(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, gin.H{
-		"Your List":   boredList,
-		"Last search": chachedBoredList,
-	})
-}
-
-func getRandomData(c *gin.Context) {
-	url := "https://www.boredapi.com/api/activity"
-	req, _ := http.NewRequest("GET", url, nil)
-	res, _ := http.DefaultClient.Do(req)
-
-	// res, err := http.Get(url)
-	// if err != nil {
-	// 	panic(err.Error())
-	// }
-
-	if res.Status == "200 OK" {
-		body, err := ioutil.ReadAll(res.Body)
-		defer res.Body.Close()
-		if err != nil {
-			panic(err.Error())
-		}
-		var data BoredList
-		json.Unmarshal(body, &data)
-		if len(chachedBoredList) > 0 {
-			chachedBoredList[0] = data
-		} else {
-			chachedBoredList = append(chachedBoredList, data)
-		}
-
-		fmt.Printf("Results: %v\n", chachedBoredList)
-
-		c.Data(http.StatusOK, "application/json", body)
-	} else {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": res.Status})
-	}
-
-}
-
-// func getBoredItem(c *gin.Context) {
-	
-// }
-
-func addMyBoredList(c *gin.Context) {
-	if len(chachedBoredList) > 0 {
-		boredList = append(boredList, chachedBoredList[0])
-		c.IndentedJSON(http.StatusCreated, chachedBoredList[0])
-	} else {
-		c.IndentedJSON(http.StatusOK, gin.H{"message": "Nothing to add"})
+func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("REQ %s %s", r.Method, r.URL.Path)
+		next(w, r) // call actual handler
+		log.Printf("RES %s %s", r.Method, r.URL.Path)
 	}
 }
 
-// server conf
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create snippet"))
+}
+
+
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/{$}", loggingMiddleware(home))  // Restrict this route to exact matches on / only
+	// mux.HandleFunc("/static/", home)  // subtree path pattern, means /static/**, the first matching handler will run
+	mux.HandleFunc("/snippet/view", loggingMiddleware(snippetView))
+	mux.HandleFunc("/snippet/create", loggingMiddleware(snippetCreate))
 
-	router := gin.Default()
+	log.Print("starting server on :4000")
 
-	// routers
-	router.GET("/mybored-list", getMyBoredList)
-	router.GET("/random-bored-thing", getRandomData)
-	// router.GET("/get-bored", getBoredItem)
-
-	router.POST("/add-bored", addMyBoredList)
-	router.POST("/remove-bored", addMyBoredList)
-
-	router.Run("localhost:8080")
+	err := http.ListenAndServe(":4000", mux)  // how this actually works under the hood?
+	log.Fatal(err) // how to do other log levels? is it used in prod apps?
 }
