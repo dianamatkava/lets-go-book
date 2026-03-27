d42ce95d98d6f26ca19ba0a1b5da3f3941365e2f
dianamatkava <88348384+dianamatkava@users.noreply.github.com>
2026-03-27 14:04:29 +0100

    introduce app struc for di

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,13 +3,12 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"./ui/html/pages/base.html",
 		"./ui/html/pages/home.html",
@@ -17,23 +16,24 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 	file, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
+		app.logger.Error(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 	err = file.ExecuteTemplate(w, "base", nil)  // write headers and body
 	if err != nil {
-		log.Println(err.Error())
+		app.logger.Error(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 }
 
-func getSnippets(w http.ResponseWriter, r *http.Request) {
+func (app *application) getSnippets(w http.ResponseWriter, r *http.Request) {
+	app.logger.Info("Display all snippets")
 	fmt.Fprint(w, "Display all snippets")
 }
 
-func getSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) getSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -42,11 +42,11 @@ func getSnippet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Get specific snippet by ID: %d", id)
 }
 
-func getSnippetCreateForm(w http.ResponseWriter, r *http.Request) {
+func (app *application) getSnippetCreateForm(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Create form")
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 	w.WriteHeader(http.StatusCreated)  // why I dont specify that it is a status code and not something else?
 	fmt.Fprintf(w, "Create snippet %d", 12)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,30 +7,40 @@ import (
 	"os"
 )
 
-func loggingMiddleware(logger *slog.Logger, next http.HandlerFunc) http.HandlerFunc {
+// application-wide dependencies
+type application struct {
+	logger *slog.Logger
+}
+
+
+func (app *application) loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		logger.Info("REQ", "Method", r.Method, "URL", r.URL.Path)
+		app.logger.Info("REQ", "Method", r.Method, "URL", r.URL.Path)
 		next(w, r) // call actual handler
-		logger.Info("RES", "Method", r.Method, "URL", r.URL.Path)
+		app.logger.Info("RES", "Method", r.Method, "URL", r.URL.Path)
 	}
 }
 
 
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	app := &application{logger: logger}
+
 	address := flag.String("adr", ":4000", "HTTP network address")
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", loggingMiddleware(logger, home))  // Restrict this route to exact matches on / only
-	// mux.HandleFunc("/static/", home)  // subtree path pattern, means /static/**, the first matching handler will run
-	mux.HandleFunc("GET /snippet", loggingMiddleware(logger, getSnippets))
-	mux.HandleFunc("GET /snippet/{id}", loggingMiddleware(logger, getSnippet))
-	mux.HandleFunc("POST /snippet", loggingMiddleware(logger, createSnippet))
-	mux.HandleFunc("GET /snippet/createForm", loggingMiddleware(logger, getSnippetCreateForm))
 
 	fs := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fs))
 
+	mux.HandleFunc("/{$}", app.loggingMiddleware(app.home))  // Restrict this route to exact matches on / only
+	// mux.HandleFunc("/static/", home)  // subtree path pattern, means /static/**, the first matching handler will run
+	mux.HandleFunc("GET /snippet", app.loggingMiddleware(app.getSnippets))
+	mux.HandleFunc("GET /snippet/{id}", app.loggingMiddleware(app.getSnippet))
+	mux.HandleFunc("POST /snippet", app.loggingMiddleware(app.createSnippet))
+	mux.HandleFunc("GET /snippet/createForm", app.loggingMiddleware(app.getSnippetCreateForm))
+	
 	logger.Info("starting server on", "address", *address)
 
 	err := http.ListenAndServe(*address, mux)  // how this actually works under the hood?
