45ba26c7b22162a4d302aca0594ecf84558a0286
dianamatkava <88348384+dianamatkava@users.noreply.github.com>
2026-04-01 13:53:47 +0200

    add db connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+	"net"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 // application-wide dependencies
 type application struct {
 	logger *slog.Logger
+	db *sql.DB
 }
 
 
@@ -21,12 +26,40 @@ func (app *application) loggingMiddleware(next http.HandlerFunc) http.HandlerFun
 	}
 }
 
+func openDB(driverName string, dataSourceName string) (*sql.DB, error) {
+	db, err := sql.Open(driverName, dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+
+	errPing := db.Ping()
+	if errPing != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
+}
+
 
 func main() {
-	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	net.Listen()
 	address := flag.String("adr", ":4000", "HTTP network address")
+	
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	
+	db, dbErr := openDB("pgx", "postgres://postgres:postgres@localhost/postgres?parseTime=true")
+	if dbErr != nil {
+		logger.Error(dbErr.Error())
+		os.Exit(1)
+	}
 
-	app := &application{logger: logger}
+	defer db.Close()
+
+	app := &application{
+		logger: logger,
+		db: db,
+	}
 	
 	logger.Info("starting server on", "address", *address)
 
